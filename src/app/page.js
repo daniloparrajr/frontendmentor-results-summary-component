34cd1a8846ba06345ba3a3365c86a68c00fd9fe8
Danilo Parra Jr. <daniloparrajr.work@gmail.com>
2024-10-16 23:46:00 +0800

    fix: add document h1 headding

--- src/app/page.js
+++ src/app/page.js
@@ -4,7 +4,7 @@ import ResultsSummary from "@/app/components/ResultsSummary";
 export default function Home() {
   return (
       <main role="main">
-          <h1 className="hidden">Frontend Mentor | Results summary component</h1>
+          <h1 className="sr-only">Frontend Mentor | Results summary component</h1>
           <ResultsSummary />
       </main>
   );
